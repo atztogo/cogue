97c990167829566035a02b15557d48f8915d77e7
Atsushi Togo <atz.togo@gmail.com>
2012-12-23 00:19:05 +0900

    Prepare directory for electronic functions

---
+++ cogue/electronic/entropy.py
@@ -0,0 +1,4 @@
+# Entropy of non-interacting electrons
+# S=-gk_{\mathrm{B}}\Sigma_i\[f_i \ln f_i + (1-f_i)\ln (1-f_i)\]
+# g: degree of degeneracy
+# f_i:probability that a state is occpied f_i = \left[1+\exp\left(\frac{E-\mu}{T}\right)\right\]^{-1}
