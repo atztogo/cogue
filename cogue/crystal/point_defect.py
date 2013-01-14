6c0162191ad134e4af177b2435bf0ffe94235700
Atsushi Togo <atz.togo@gmail.com>
2013-01-14 17:36:49 +0900

    Implement point_defect.py

--- cogue/crystal/point_defect.py
+++ cogue/crystal/point_defect.py
@@ -1,2 +1,29 @@
+import sys
 from cogue.crystal.builder import CellBuilder
+from cogue.crystal.cell import Cell
 
+class PointDefect(Cell):
+    def __init__(self, cell):
+        self._original_cell = cell.copy()
+        self.set_cell(cell)
+
+    def set_cell(self, cell):
+        Cell.__init__(self,
+                      lattice=cell.get_lattice(),
+                      magmoms=cell.get_magnetic_moments(),
+                      masses=cell.get_masses(),
+                      numbers=cell.get_numbers(),
+                      points=cell.get_points())
+        
+    def set_point_vacancy(self, index):
+        n = len(self._symbols)
+        if index < 0 or index > n - 1:
+            sys.stderr.write(
+                "At least a pair of point and symbol or "
+                "a pair of point and number have to be set.\n")
+        else:
+            builder = CellBuilder(self._original_cell)
+            builder.pop(index)
+            self.set_cell(builder.get_cell())
+            
+            
---
+++ test/point_defect.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cogue
+import cogue.calculator.vasp as vasp
+from cogue.crystal.point_defect import PointDefect
+from cogue.crystal.supercell import get_supercell
+
+symbols = ['Cu'] * 4
+lattice = [[3.61, 0, 0],
+           [0, 3.61, 0],
+           [0, 0, 3.61]] # Orthorhombic
+points=np.transpose([[0.0, 0.0, 0.0],
+                     [0.0, 0.5, 0.5],
+                     [0.5, 0.0, 0.5],
+                     [0.5, 0.5, 0.0]])
+cell = cogue.cell(lattice=lattice,
+                  points=points,
+                  symbols=symbols)
+supercell = get_supercell(cell, np.diag([2, 2, 2]))
+vacancy_cell = PointDefect(supercell)
+vacancy_cell.set_point_vacancy(3)
+# To use get_cell restricts the cell to be an object of Cell but not CellBuilder
+vasp.write_poscar(vacancy_cell)
